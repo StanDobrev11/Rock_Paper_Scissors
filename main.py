e1587ec5aeaccff2160c6211e9bdf77a7f9d958b
StanDobrev11 <dobrev81@gmail.com>
2023-09-21 04:01:21 +0000

    'update'

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ player_move = input('Please make your move [r]ock, [p]aper, [s]cissors or [e]xit
 computer_move = random.choice(move_list)
 
 while player_move != 'e':
+
+    valid_key = False
+    while not valid_key:
+        try:
+            valid_key = True if win_conditions[player_move] else False
+        except KeyError:
+            print('Wrong input. Try again')
+            player_move = input('Please make your move [r]ock, [p]aper, [s]cissors or [e]xit ')
+
     if win_conditions[player_move] == computer_move:
         score['player'] += 1
         print(f'Player: {mapper[player_move]}')
@@ -32,14 +41,15 @@ while player_move != 'e':
         print(f'Computer: {mapper[computer_move]}')
         print('Round won by computer')
     else:
-        print(f'Player: {player_move}')
-        print(f'Computer: {computer_move}')
+        print(f'Player: {mapper[player_move]}')
+        print(f'Computer: {mapper[player_move]}')
         print('Round tie')
+
     games_played += 1
     player_move = input('Please make your move [r]ock, [p]aper, [s]cissors or [e]xit ')
     computer_move = random.choice(move_list)
 
-print(f'Total round {games_played}')
+print(f'Total rounds played: {games_played}')
 print('Score:')
 for k, v in score.items():
     print(f'{k}: {v}')
